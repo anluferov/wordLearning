413a0b307c68bc52a4f6e20cbeb16331742451cb
Andrey Luferau <anluferov@gmail.com>
2022-03-27 15:01:15 +0700

    Initial Commit

---
+++ wordLearning/wordLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordLearningApp.swift
+//  wordLearning
+//
+//  Created by Andrey Luferau on 3/27/22.
+//
+
+import SwiftUI
+
+@main
+struct wordLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
